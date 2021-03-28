e19ddebc9cdb69667a25df3250ff289afad6218c
homeless1337 <72689483+homeless1337@users.noreply.github.com>
2021-03-28 07:28:21 -0700

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,101 @@
+/*How to become proffesional hacker. Learn c++ the right way. This will help you learn c++ the easy way. After you are done understanding this code here is what you have achieved -> 
+1. Nothing.
+2. Nothing.
+3. Nothing.
+
+  Below are some questions you will ask. So i answer it here.
+
+  Question 1. What can i do after learning this code?
+  Question 2. What does std::cout does?
+  Question 3. IDK.
+
+  **Answers**
+  * Answer 1. After learning this code you can hack nasa and hack into any system you wish and make anything in the world with programming you want.
+  * Answer 2. I don't even know.
+  * Answer 3. IDK.
+  
+  Make sure to add comments above all classes like i am doing below (This will make people think your a proffesional programmer).
+*/
+
+/*File Name : ConsoleApplication1.cpp
+  Purpose : Main
+*/
+
+/*Includes*/
+#include <Windows.h>
+#include <iostream>
+
+/*Library includes.*/
+//none atm
+
+/*Main start*/
+int main()
+{
+	std::string str1;  // define
+	str1 = "w";  // define
+	std::string str2;  // define
+	str2 = "e";  // define
+	std::string str3;  // define
+	str3 = "L";  // define
+	std::string str4;  // define
+	str4 = "i";  // define
+	std::string str5;  // define
+	str5 = "k";  // define
+	std::string str6;  // define
+	str6 = "e";  // define
+	std::string str7;  // define
+	str7 = "f";  // define
+	std::string str8;  // define
+	str8 = "o";  // define
+	std::string str9;  // define
+	str9 = "r";  // define
+	std::string str10;  // define
+	str10 = "t";  // define
+	std::string str11;  // define
+	str11 = "n";  // define
+	std::string str12;  // define
+	str12 = "i";  // define
+	std::string str13;  // define
+	str13 = "t";  // define
+	std::string str14;  // define
+	str14 = "e";  // define
+
+	std::string main1;  // define
+	main1 = str1 + str2;  // define
+	std::string main2;  // define
+	main2 = str3 + str4;  // define
+	std::string main3;  // define
+	main3 = str5 + str6;  // define
+	std::string main4;  // define
+	main4 = str7 + str8;  // define
+	std::string main5;  // define
+	main5 = str9 + str10;  // define
+	std::string main6;  // define
+	main6 = str11 + str12;  // define
+	std::string main7;  // define
+	main7 = str13 + str14;  // define
+
+	std::string final1;  // define
+	final1 = main1 + main2;  // define
+	std::string final2;  // define
+	final2 = main3 + main4;  // define
+	std::string final3;  // define
+	final3 = main5 + main6;  // define
+	std::string final4;  // define
+	final4 = main7;  // define
+
+	std::string final_final1;  // define
+	final_final1 = final1 + final2;  // define
+	std::string final_final2;  // define
+	final_final2 = final3 + final4;  // define
+
+	std::string final_final_final1;  // define
+	final_final_final1 = final_final1 + final_final2;  // define
+
+	std::string final_final_final_final = final_final_final1;  // define
+
+	std::cout << final_final_final_final;
+	getchar();
+}
+
+/*APPLICATION ENDER.*/
